005d57a89d7d409b351e6b552ff373503c20d395
Gerson <gersonrodriguessantos8@gmail.com>
2024-08-26 23:23:47 -0300

    feat: config locals file

--- locals.tf
+++ locals.tf
@@ -3,6 +3,75 @@ locals {
   domain_full = format("qdrant.%s.%s", trimprefix("${var.subdomain}.${var.cluster_name}", "."), var.base_domain)
 
   helm_values = [{
-    qdrant = {}
+    qdrant = {
+      replicaCount = 1
+      # nameOverride = ""
+      # fullnameOverride = ""
+
+      ingress = {
+        enabled = true
+        ingressClassName : ""
+        annotations = {
+          "cert-manager.io/cluster-issuer"                   = "${var.cluster_issuer}"
+          "traefik.ingress.kubernetes.io/router.entrypoints" = "websecure"
+          "traefik.ingress.kubernetes.io/router.tls"         = "true"
+        }
+        hosts = [
+          {
+            host = local.domain
+            paths = [{
+              path        = "/"
+              pathType    = "Prefix"
+              servicePort = 6333
+            }]
+          },
+          {
+            host = local.domain_full
+            paths = [{
+              path        = "/"
+              pathType    = "Prefix"
+              servicePort = 6333
+            }]
+          }
+        ]
+        tls = [{
+          secretName = "minio-tls"
+          hosts = [
+            local.domain,
+            local.domain_full,
+          ]
+        }]
+      }
+
+      resources : {
+        limits = {
+          cpu    = "1000m"
+          memory = "1Gi"
+        }
+        requests = {
+          cpu    = "100m"
+          memory = "128Mi"
+        }
+      }
+
+      persistence = {
+        size = "10Gi"
+      }
+
+      config = {
+        cluster = {
+          enabled : true
+        }
+      }
+
+      metrics = {
+        serviceMonitor = {
+          enabled : var.enable_service_monitor
+        }
+      }
+
+      apiKey         = true
+      readOnlyApiKey = true
+    }
   }]
 }
